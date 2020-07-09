65378bd9ab099b4996490a76b149d82dc66f77ae
Vadim <v.dudko93@gmail.com>
2020-07-09 15:25:35 +0300

    feat: add components

---
+++ react-apps/todo/src/components/todo-list.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import TodoListItem from './todo-list-item';
+
+const TodoList = ({ todos }) => {
+
+    const elements = todos.map((item) => {
+
+        const { id, ...itemProps  } = item;
+
+        return (
+            <li key={item.id}>
+                <TodoListItem { ...itemProps } />
+            </li>
+        );
+    });
+
+    return (
+        <ul>
+            { elements }
+        </ul>
+    );
+};
+
+export default TodoList;
